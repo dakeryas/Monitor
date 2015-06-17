da81bb3c67f13ec8da54772d95e7ee302e059991
Dakeryas <mrdakeryas@gmail.com>
2015-06-17 11:54:40 +0200

    Created Point<T>

--- include/Point.hpp
+++ include/Point.hpp
@@ -1,25 +1,88 @@
 #ifndef POINT_H
 #define POINT_H
 
-#include <cmath>
+#include <vector>
+#include <initializer_list>
 #include <iostream>
-#include <TRandom3.h>
+#include <iomanip>
 
+template <class T>
 class Point {
   
 public:
-  double x, y, z;
-  
-  Point();
-  Point(double x, double y, double z);
-  Point(TRandom3& ran, double R, double h); //generates a point randomly into a cylinder of radius r and height h
-  Point(TRandom3& ran, double R1, double R2, double h1, double h2); //generates a point randomly between two cylinders of respective radii R1 and R2 and of respective height h1 and h2
-  virtual ~Point();
-  void cylinder(TRandom3& ran, double R, double h); //moves a point randomly into a cylinder of radius r and height h
-  void HollowCylinder(TRandom3& ran, double R1, double R2, double h1, double h2); //moves a point randomly between two cylinders of respective radii R1 and R2 and of respective height h1 and h2
+  std::vector<T> coordinates;
+  template <class Iterator>
+  Point(Iterator firstCoordinate, Iterator lastCoordinate);
+  Point(std::initializer_list<T> coordinates);
+  std::vector<T>::const_iterator begin() const;
+  std::vector<T>::const_iterator end() const;
+  std::vector<T>::iterator begin() ;
+  std::vector<T>::iterator end() ;
+  unsigned getDimension() const;
   
 };
 
-std::ostream& operator<<(std::ostream& output, const Point& p);
+template <class T>
+std::ostream& operator<<(std::ostream& output, const Point<T>& point){
+  
+  std::ostream output;
+  output<<"(";
+  
+  if(point.getDimension() != 0){
+    
+    for(auto it = point.begin(); it != point.end() - 1; ++it) output<<std::setw(4)<<std::internal<<*it<<", ";
+    output<<(point.end() -1);
+    
+  }
+  
+  output<<")";
+  return output;
+  
+}
+
+template <class T>
+Point<T>::Point(Iterator firstCoordinate, Iterator lastCoordinate):coordinates(firstCoordinate, lastCoordinate){
+
+}
+
+template <class T>
+Point<T>::Point(std::initializer_list<T> coordinates):Point(coordinates.begin, coordinates.end()){
+
+}
+
+template <class T>
+std::vector< T >::const_iterator Point<T>::begin() const{
+  
+  return coordinates.begin();
+
+}
+
+template <class T>
+std::vector< T >::const_iterator Point<T>::end() const{
+  
+  return coordinates.end();
+
+}
+
+template <class T>
+std::vector< T >::iterator Point<T>::begin(){
+  
+  return coordinates.begin();
+
+}
+
+template <class T>
+std::vector< T >::iterator Point<T>::end(){
+  
+  return coordinates.end();
+
+}
+
+template <class T>
+unsigned Point<T>::getDimension() const{
+  
+  return coordinates.size();
+
+}
 
 #endif
