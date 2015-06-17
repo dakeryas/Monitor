dcd200d55f56122882dd7f9692dd4cc4bb6382f7
Dakeryas <mrdakeryas@gmail.com>
2015-06-17 14:37:04 +0200

    Added Histogram::begin and Histogram::end to use ranged-based loops

--- include/Histogram.hpp
+++ include/Histogram.hpp
@@ -17,22 +17,27 @@ public:
   Histogram<T,K>& operator*=(const K& factor);
   Histogram<T,K>& operator/=(const K& factor);
   Histogram<T,K>& normalise();
+  typename std::map<Bin<T>,K>::const_iterator begin() const;
+  typename std::map<Bin<T>,K>::const_iterator end() const;
+  typename std::map<Bin<T>,K>::iterator begin();
+  typename std::map<Bin<T>,K>::iterator end();
+  K getCount(const T& value) const;
+  K getCount(const Bin<T>& bin) const;
+  K getTotalCounts() const;
+  const std::map<Bin<T>, K>& getCountMap() const;
   void addChannel(const Bin<T>& bin);
   template <class Iterator>
   void addChannels(Iterator begin, Iterator end);//copy channels pointed to from begin to end
   void addCount(const T& value);
   void setCount(const Bin<T>& bin, const K& count);
-  K getCount(const T& value) const;
-  K getCount(const Bin<T>& bin) const;
-  K getTotalCounts() const;
-  const std::map<Bin<T>, K>& getCountMap() const;
+  
   
 };
 
 template <class T, class K>
 std::ostream& operator<<(std::ostream& output, const Histogram<T,K>& histogram){
   
-  for(const auto& pair : histogram.getCountMap()) output<<pair.first<<std::setw(6)<<std::left<<" "<<"-->"<<std::setw(6)<<std::left<<" "<<std::setw(9)<<std::left<<pair.second<<"\n";
+  for(const auto& pair : histogram) output<<pair.first<<std::setw(6)<<std::left<<" "<<"-->"<<std::setw(6)<<std::left<<" "<<std::setw(9)<<std::left<<pair.second<<"\n";
   return output;
   
 }
@@ -104,6 +109,57 @@ Histogram<T,K>& Histogram<T,K>::normalise(){
 }
 
 template <class T, class K>
+typename std::map<Bin<T>,K>::const_iterator Histogram<T,K>::begin() const{
+
+  return countMap.begin();
+  
+}
+
+template <class T, class K>
+typename std::map<Bin<T>,K>::const_iterator Histogram<T,K>::end() const{
+
+  return countMap.end();
+  
+}
+
+template <class T, class K>
+typename std::map<Bin<T>,K>::iterator Histogram<T,K>::begin(){
+
+  return countMap.begin();
+  
+}
+
+template <class T, class K>
+typename std::map<Bin<T>,K>::iterator Histogram<T,K>::end(){
+
+  return countMap.end();
+  
+}
+
+template <class T, class K>
+K Histogram<T,K>::getCount(const T& value) const{
+  
+  for(auto& pair : countMap) if(pair.first.contains(value)) return pair.second;
+  
+}
+
+template <class T, class K>
+K Histogram<T,K>::getCount(const Bin<T>& bin) const{
+  
+  return countMap.at(bin);
+  
+}
+
+template <class T, class K>
+K Histogram<T,K>::getTotalCounts() const{
+
+  K totalCounts = 0;
+  for(const auto& pair : countMap) totalCounts += pair.second;
+  return totalCounts;
+  
+}
+
+template <class T, class K>
 void Histogram<T,K>::addChannel(const Bin<T>& bin){
   
   countMap.emplace(bin, K{});//default construct K
@@ -132,34 +188,5 @@ void Histogram<T,K>::setCount(const Bin<T>& bin, const K& count){
   
 }
 
-template <class T, class K>
-K Histogram<T,K>::getCount(const T& value) const{
-  
-  for(auto& pair : countMap) if(pair.first.contains(value)) return pair.second;
-  
-}
-
-template <class T, class K>
-K Histogram<T,K>::getCount(const Bin<T>& bin) const{
-  
-  return countMap.at(bin);
-  
-}
-
-template <class T, class K>
-K Histogram<T,K>::getTotalCounts() const{
-
-  K totalCounts = 0;
-  for(const auto& pair : countMap) totalCounts += pair.second;
-  return totalCounts;
-  
-}
-
-template <class T, class K>
-const std::map<Bin<T>, K>& Histogram<T,K>::getCountMap() const{
-  
-  return countMap;
-
-}
 
 #endif
