918ac78120a9671a13e06a9863fc47e9e6727b11
Dakeryas <mrdakeryas@gmail.com>
2015-07-06 15:20:37 +0200

    Separate implementation for the functions declared in Constants.hpp

--- include/Constants.hpp
+++ include/Constants.hpp
@@ -67,32 +67,9 @@ namespace constants{
     
   }
   
-  void adaptUnits(double& runLenght, double& power1, double& power2){//convert values to days and GW
-  
-    runLenght *= time::secondToDay;
-    power1 *= energy::MWattToGWatt;
-    power2 *= energy::MWattToGWatt;
-  
-  }
-  
-  double crossSection(double neutrinoEnergy){
-    
-    if(neutrinoEnergy > (mass::neutron - mass::proton)){//if we pass the threshold
-      
-      double positronEnergy = neutrinoEnergy - mass::neutron + mass::proton;
-      return positronEnergy * std::sqrt(std::pow(positronEnergy, 2) - std::pow(mass::electron, 2));
-      
-    }
-    else return 0;//if the neutrino's energy is too low, the cross section is zero
-    
-  }
-  
-  double oscillation(double neutrinoEnergy, double distance, double th13 = mixing::th13, double delta31 = squaredMass::delta31){//energy in MeV and distance in m
-    
-    
-    return 1 - std::pow(std::sin(2 * th13) * std::sin(1.27 * delta31 * distance / neutrinoEnergy), 2);
-    
-  }
+  void adaptUnits(double& runLenght, double& power1, double& power2);//convert values to days and GW
+  double crossSection(double neutrinoEnergy);
+  double oscillation(double neutrinoEnergy, double distance, double th13 = mixing::th13, double delta31 = squaredMass::delta31);//energy in MeV and distance in m
   
 }
 
