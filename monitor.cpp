c224d59cb220a15261b38ca733193a53b6bff9fb
Dakeryas <mrdakeryas@gmail.com>
2015-07-13 12:08:24 +0200

    Simple monitor usage

--- monitor.cpp
+++ monitor.cpp
@@ -1,97 +1,100 @@
+#include <sstream>
 #include "TFile.h"
-#include "TTree.h"
-#include "TH1D.h"
-#include "Reactor.hpp"
-#include "Constants.hpp"
-#include "ExperimentConverter.hpp"
+#include "TCanvas.h"
+#include "ExperimentExtractor.hpp"
+#include "Converter.hpp"
 #include "Binner.hpp"
+#include "Simulation.hpp"
 
-void neutrinoRetriever(TTree* data, TTree* simu1, TTree* simu2, double energyThreshold, const char* outname){
+void neutrinoRetriever(TTree* data, TTree* simu1, TTree* simu2, const char* outname, const std::vector<Histogram<double, double>>& referenceSpectra){
   
-  int runData;
-  double currentEnergy;
-  data->SetBranchAddress("RunNumber", &runData);
-  data->SetBranchAddress("myPromptEvisID", &(currentEnergy));
+  ExperimentExtractor experimentExtractor(data, simu1, simu2);
   
-  int runSimu;
-  double runLength, numberOfNeutrinosSimu1, power1, f239Pu_1, f241Pu_1, f235U_1, f238U_1;
-  simu1->SetBranchAddress("run", &runSimu);
-  simu1->SetBranchAddress("runlength", &runLength);
-  simu1->SetBranchAddress("n_nu", &numberOfNeutrinosSimu1);
-  simu1->SetBranchAddress("p_th", &power1);
-  simu1->SetBranchAddress("f239Pu", &f239Pu_1);
-  simu1->SetBranchAddress("f241Pu", &f241Pu_1);
-  simu1->SetBranchAddress("f235U", &f235U_1);
-  simu1->SetBranchAddress("f238U", &f238U_1);
-  double numberOfNeutrinosSimu2, power2, f239Pu_2, f241Pu_2, f235U_2, f238U_2;
-  simu2->SetBranchAddress("n_nu", &numberOfNeutrinosSimu2);
-  simu2->SetBranchAddress("p_th", &power2);
-  simu2->SetBranchAddress("f239Pu", &f239Pu_2);
-  simu2->SetBranchAddress("f241Pu", &f241Pu_2);
-  simu2->SetBranchAddress("f235U", &f235U_2);
-  simu2->SetBranchAddress("f238U", &f238U_2);
+  Binner<double> binner({Axis<double>(5, 0.44, 0.59), Axis<double>(1, 0.086, 0.089), Axis<double>(5, 0.26, 0.4), Axis<double>(2, 0.03, 0.08)});
+  auto experiment = experimentExtractor.extractExperiment<double>(constants::distance::L1, constants::distance::L2, constants::backgroundRate::total, binner.generateBinning());
+//   experiment.slim();
+//   std::cout<<experiment<<std::endl;
   
-  std::vector<Particle> neutrinos;
-  Reactor reactor1, reactor2,  equivalentReactor;
-  reactor1.setDistanceToDetector(constants::distance::L1);
-  reactor2.setDistanceToDetector(constants::distance::L2);
-
-  Experiment<double> experiment(constants::distance::L1, constants::distance::L2, constants::backgroundRate::total);
-  auto channels = Binner::createBinning(6, 0.28, 0.4);
-  experiment.addChannels(channels.begin(), channels.end());
-  
-  unsigned i = 0;
-  data->GetEntry(i);
-  
-  for(unsigned k = 0; k<simu1->GetEntries(); ++k){//simu1 and simu2 have the same number of entries
-
-    simu1->GetEntry(k);
-    simu2->GetEntry(k);
-    
-    constants::adaptUnits(runLength, power1, power2);
-    
-    while(runData == runSimu && i < data->GetEntries()-1){
-      
-      if(currentEnergy > energyThreshold){
-	
-	neutrinos.emplace_back(Particle(currentEnergy));
-	
-      }
-      data->GetEntry(++i);
-    
-    }
-    
-    reactor1.setPower(power1);
-    reactor1.setFuel(Fuel(f235U_1,f238U_1,f239Pu_1,f241Pu_1));
-    reactor2.setPower(power2);
-    reactor2.setFuel(Fuel(f235U_2,f238U_2,f239Pu_2,f241Pu_2));
-    equivalentReactor = reactor1 + reactor2;
-    
-    experiment.addRun(equivalentReactor.getFuel().getFrac("239Pu"), Run(neutrinos.begin(), neutrinos.end(), runLength, power1, power2));
-    
-    neutrinos.resize(0);
-
-  }
-
-  std::cout<<experiment<<std::endl;
-  auto energyChannels = Binner::createBinning(4, 0., 8.);
-  for(const auto& pair : experiment.getRunMap()) std::cout<<pair.first<<"\n"<<pair.second.getNeutrinoSpectrum<double, double>(energyChannels.begin(), energyChannels.end()).normalise()<<std::endl;
+  Simulation<double, double> simulation(constants::distance::average, constants::mixing::th13, constants::squaredMass::delta31, referenceSpectra.begin(), referenceSpectra.end());
+  simulation.simulateToMatch(experiment);
+  simulation.shiftResultingSpectra(constants::mass::proton - constants::mass::neutron  + constants::mass::electron);//convert the neutrino's energy to the positron's energy + electron's annihilation mass
+//   std::cout<<simulation<<std::endl;
   
   TFile outfile(outname, "recreate");
-  ExperimentConverter::TGraph(experiment).Write("graph");
+  auto rate = Converter::toTH1(experiment.getRateHistogram<double,double>().integrateDimensions({0,1,3}));
+  if(rate){
   
+    if(rate->GetDimension() == 1){
+      
+      rate->SetLineColor(kBlue);
+      rate->SetLineWidth(2);
+      rate->Fit("pol1", "NEXO");
+      
+    }
+    else rate->SetFillColor(kBlue);
+    
+    rate->Write("rate");
+    
+  }
+  
+  Point<double> referenceConfiguration{0.575, 0.0875, 0.274, 0.0425};//U5,U8,PU9,PU41
+  
+  TGraphErrors spectrum;
+  auto normaliser = simulation.getResulingSpectrum(referenceConfiguration);
+  unsigned index{};
+  for(auto pair : simulation.getResults()){
+    
+    pair.second /= normaliser;
+    spectrum = Converter::toTGraph(pair.second);
+    spectrum.SetName(("spectrum_simu_"+std::to_string(index)).c_str());
+    spectrum.SetTitle(Converter::toString(pair.first.getCenter()).c_str());
+    spectrum.SetLineColor(index + 1);
+    spectrum.SetLineWidth(2);
+    spectrum.Write();
+
+    ++index;
+    
+  }
+  
+  Histogram<double,double> energyHistogram;
+  binner.setAxes(4, 0., 8.);
+  auto energyChannels = binner.generateBinning();
+  normaliser = experiment.getNeutrinoSpectrum<double,double>(referenceConfiguration, energyChannels);
+  index = 0;
+  for(const auto& pair : experiment){
+   
+    energyHistogram = pair.second.getScaledNeutrinoSpectrum<double,double>(experiment.getDistance1(), experiment.getDistance2(), experiment.getBackgroundRate(),energyChannels);
+    energyHistogram /= normaliser;
+    spectrum = Converter::toTGraph(energyHistogram);
+    spectrum.SetName(("spectrum_data_"+std::to_string(index)).c_str());
+    spectrum.SetTitle(Converter::toString(pair.first.getCenter()).c_str());
+    spectrum.SetLineColor(index + 1);
+    spectrum.SetLineWidth(2);
+    spectrum.Write();
+  
+    ++index;
+  
+  }
 }
 
 void monitor(const std::string& dirname, const std::string& outname){
   
   TFile dataFile((dirname+"/"+"FinalFitIBDTree_DCIII_Data.root").c_str());
   TFile simuFile1((dirname+"/"+"nu_rate_per_run_B1_3rdPub_f.root").c_str());
-  TFile simuFile2((dirname+"/"+"nu_rate_per_run_B2_3rdPub_f.root").c_str()); 
+  TFile simuFile2((dirname+"/"+"nu_rate_per_run_B2_3rdPub_f.root").c_str());
+  
+  TFile referenceSpectraFile((dirname+"/"+"reference.root").c_str());
+  std::vector<Histogram<double, double>> referenceSpectra(4);
+  referenceSpectra[0] = Converter::toHistogram<double,double>(*dynamic_cast<TH1D*>(referenceSpectraFile.Get("U235")));
+  referenceSpectra[1] = Converter::toHistogram<double,double>(*dynamic_cast<TH1D*>(referenceSpectraFile.Get("U238")));
+  referenceSpectra[2] = Converter::toHistogram<double,double>(*dynamic_cast<TH1D*>(referenceSpectraFile.Get("Pu239")));
+  referenceSpectra[3] = Converter::toHistogram<double,double>(*dynamic_cast<TH1D*>(referenceSpectraFile.Get("Pu241")));
+  
   TTree* data = dynamic_cast<TTree*>(dataFile.Get("FinalFitIBDTree"));
   TTree* simu1 = dynamic_cast<TTree*>(simuFile1.Get("nu"));
   TTree* simu2 = dynamic_cast<TTree*>(simuFile2.Get("nu"));
   
-  neutrinoRetriever(data, simu1, simu2, 0, outname.c_str());
+  neutrinoRetriever(data, simu1, simu2, outname.c_str(), referenceSpectra);
   
 }
 
